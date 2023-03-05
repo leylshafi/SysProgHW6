a305d6b1b082467e931286c68a00ca27e2038523
Leyla Shafiyeva <llshafi03@gmail.com>
2023-03-05 14:35:14 +0400

    focus

--- SysProgHW6/MainWindow.xaml.cs
+++ SysProgHW6/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ public partial class MainWindow : Window
     ObservableCollection<string> words = new ObservableCollection<string>();
     public ObservableCollection<string> fittingWords { get; set; }
     bool allow=false;
+    int i = 0;
     public MainWindow()
     {
         InitializeComponent();
@@ -31,8 +32,12 @@ public partial class MainWindow : Window
 
     private void Button_Click(object sender, RoutedEventArgs e)
     {
-        if(!words.Contains(tb.Text))
+        if (!words.Contains(tb.Text))
+        {
             words.Add(tb.Text);
+            tb.Text= string.Empty;
+        }
+           
     }
 
     private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -42,6 +47,8 @@ public partial class MainWindow : Window
 
     private void Button_Click_2(object sender, RoutedEventArgs e)
     {
+        tb.Text = tb.Text;
+        fittingWords.Clear();
         //if(tb.SelectedItem is not null)
         //{
         //    tb.Text= tb.SelectedItem.ToString();
@@ -73,8 +80,38 @@ public partial class MainWindow : Window
                     if (word.ToLower().StartsWith(tb.Text.ToLower()))
                         fittingWords.Add(word);
                 }
-                
+
+                //if (fittingWords.Count > 0 && allow == true)
+                //{
+                //    var selectedWord = fittingWords[0];
+
+                //    var startIndex = tb.Text.Length;
+                //    var lenght = selectedWord.Length - tb.Text.Length;
+
+                //    tb.Text += selectedWord.Remove(0, startIndex);
+                //    tb.Select(startIndex, lenght);
+                //}
+                //allow = false;
             });
         });
+    }
+
+    private void Button_Click_4(object sender, RoutedEventArgs e)
+    {
+        var selectedWord = fittingWords[i];
+        var startIndex = tb.Text.Length;
+        var length = selectedWord.Length - tb.Text.Length;
+        lb.SelectedIndex = i;
+        i++; 
+        tb.Text=lb.SelectedItem.ToString(); 
+        tb.SelectionBrush = Brushes.Gray;
+
+        tb.Select(startIndex, length);
+        tb.Focus();
+    }
+
+    private void lb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        //tb.Text = lb.SelectedItem.ToString();
     }
 }
